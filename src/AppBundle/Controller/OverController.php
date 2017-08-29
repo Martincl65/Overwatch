befb23199f4f51ac6679490de6813b1b44efcba1
Martincl65 <martin.clement65@gmail.com>
2017-08-29 09:54:23 +0200

    retest

---
+++ src/AppBundle/Controller/OverController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class OverController extends Controller
+{
+
+    public function indexAction($name)
+    {
+        $curl = curl_init("http://ow-api.herokuapp.com/profile/pc/eu/".$name);
+        curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt ( $curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC );
+        curl_setopt ( $curl, CURLOPT_SSL_VERIFYPEER, false );
+        $json = curl_exec ( $curl );
+        $obj = json_decode ( $json, true );
+        curl_close ( $curl );
+
+        $curl = curl_init("https://owapi.net/api/v3/u/".$name."/blob");
+        curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt ( $curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC );
+        curl_setopt ( $curl, CURLOPT_SSL_VERIFYPEER, false );
+        curl_setopt($curl,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+        $json = curl_exec ( $curl );
+        $stats = json_decode ( $json, true );
+        curl_close ( $curl );
+
+        return $this->render('AppBundle:default:index.html.twig', array('api' => $obj, 'stats' => $stats));
+    }
+}
