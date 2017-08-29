ba263ab0dab449b37b21f66c94c6c79368b8ecb5
Martincl65 <martin.clement65@gmail.com>
2017-08-29 10:21:15 +0200

    test commit

--- src/AppBundle/Controller/OverController.php
+++ src/AppBundle/Controller/OverController.php
@@ -11,7 +11,7 @@ class OverController extends Controller
 
     public function indexAction($name)
     {
-        /*test*/
+        /*gros test*/
         $curl = curl_init("http://ow-api.herokuapp.com/profile/pc/eu/".$name);
         curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, true );
         curl_setopt ( $curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC );
